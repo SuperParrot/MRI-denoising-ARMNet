06bc0afd68e162a4611b67345644a21c19c9ff97
SuperParrot <140707661+SuperParrot@users.noreply.github.com>
2023-12-08 13:57:23 +0800

    Add files via upload

---
+++ v1/Models/64_64/Proposed/models/DCT_Branch.py
@@ -0,0 +1,176 @@
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+import scipy.io as scio
+
+
+class DCT2d(nn.Module):
+    def __init__(self, H, W):
+        super(DCT2d, self).__init__()
+
+        self.dct_C = torch.zeros([H, W])
+        N=W
+        self.dct_C[0, :] = 1 * torch.sqrt(1 / N * torch.ones(H))
+
+        for i in range(1, H):
+            for j in range(W):
+                self.dct_C[i, j] = torch.cos(torch.tensor(3.1415926 * i * (2 * j + 1) / (2 * N))) * torch.sqrt(
+                    torch.tensor(2 / N))
+
+        self.dct_C_t=torch.transpose(self.dct_C, 0, 1)
+
+    def forward(self, x):
+        B,C,H,W=x.shape
+        x=x.flatten(start_dim=0, end_dim=1)
+
+        y = torch.zeros_like(x)
+
+        if (torch.cuda.is_available()):
+            self.dct_C=self.dct_C.cuda()
+            self.dct_C_t=self.dct_C_t.cuda()
+            y = y.cuda()
+
+        for i in range(B*C):
+            y_tmp = torch.mm(self.dct_C, x[i])
+            y[i] = torch.mm(y_tmp, self.dct_C_t)
+
+        y=y.reshape([B,C,H,W])
+        return y
+
+class IDCT2d(nn.Module):
+    def __init__(self, H, W):
+        super(IDCT2d, self).__init__()
+
+        self.dct_C = torch.zeros([H, W])
+        N=W
+        self.dct_C[0, :] = 1 * torch.sqrt(1 / N * torch.ones(H))
+
+        for i in range(1, H):
+            for j in range(W):
+                self.dct_C[i, j] = torch.cos(torch.tensor(3.1415926 * i * (2 * j + 1) / (2 * N))) * torch.sqrt(
+                    torch.tensor(2 / N))
+
+        self.dct_C_t=torch.transpose(self.dct_C, 0, 1)
+
+    def forward(self, x):
+        B,C,H,W=x.shape
+        x=x.flatten(start_dim=0, end_dim=1)
+
+        y = torch.zeros_like(x)
+
+        if (torch.cuda.is_available()):
+            self.dct_C=self.dct_C.cuda()
+            self.dct_C_t=self.dct_C_t.cuda()
+            y = y.cuda()
+
+        for i in range(B*C):
+            y_tmp = torch.mm(self.dct_C_t, x[i])
+            y[i] = torch.mm(y_tmp, self.dct_C)
+
+        y=y.reshape([B,C,H,W])
+        return y
+
+class LearnableDCTNet(nn.Module):
+    def __init__(self):
+        super(LearnableDCTNet, self).__init__()
+
+        if (torch.cuda.is_available()):
+            self.device = torch.device("cuda")
+        else:
+            self.device = torch.device("cpu")
+        
+        #Load quantization matrix
+        qmat_mat=scio.loadmat('./utils/matlab/Qmat/qmats.mat')
+        self.qmat_64=nn.Parameter(torch.tensor(qmat_mat['qmat_64']).float(), requires_grad=True)
+
+        self.qmat_64_unfold=nn.Unfold(kernel_size=(self.qmat_64.shape[1], self.qmat_64.shape[2]), stride=(self.qmat_64.shape[1], self.qmat_64.shape[2]))
+        self.qmat_64_fold = nn.Fold(kernel_size=(self.qmat_64.shape[1], self.qmat_64.shape[2]),stride=(self.qmat_64.shape[1], self.qmat_64.shape[2]), output_size=(64, 64))
+
+        self.qmat_64_dct2d=DCT2d(self.qmat_64.shape[1], self.qmat_64.shape[2])
+        self.qmat_64_idct2d = IDCT2d(self.qmat_64.shape[1], self.qmat_64.shape[2])
+
+        self.qmat64_weights = nn.Parameter(torch.ones(self.qmat_64.shape[0]), requires_grad=True)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight.data, mode='fan_in', nonlinearity='leaky_relu')
+                if(m.bias!=None):
+                    nn.init.constant_(m.bias.data, 0)
+            elif isinstance(m, nn.GroupNorm):
+                nn.init.constant_(m.weight.data, 1)
+                nn.init.constant_(m.bias.data, 0)
+    
+    def forward(self, x):
+        B,C,H,W=x.shape
+
+        if(torch.cuda.is_available()):
+            self.qmat_64=self.qmat_64.cuda()
+
+            self.qmat64_weights=self.qmat64_weights.cuda()
+
+        x_patched_qmat64=self.qmat_64_unfold(x)
+        x_patched_qmat64=x_patched_qmat64.reshape(B * C, self.qmat_64.shape[1], self.qmat_64.shape[2], (H // self.qmat_64.shape[1]) * (W // self.qmat_64.shape[2]))
+        x_patched_qmat64=x_patched_qmat64.permute(0,3,1,2)
+
+        y_qmat64_lowFreq, y_qmat64_highFreq=self.perform_dct(x_patched_qmat64, self.qmat_64_dct2d, self.qmat_64_idct2d, self.qmat_64, self.qmat64_weights)
+        y_qmat64_lowFreq=y_qmat64_lowFreq.permute(0,2,3,1)
+        y_qmat64_lowFreq=y_qmat64_lowFreq.reshape(B, C*self.qmat_64.shape[1]*self.qmat_64.shape[2], (H // self.qmat_64.shape[1]) * (W // self.qmat_64.shape[2]))
+        y_qmat64_lowFreq=self.qmat_64_fold(y_qmat64_lowFreq)
+
+        y_qmat64_highFreq = y_qmat64_highFreq.permute(0, 2, 3, 1)
+        y_qmat64_highFreq = y_qmat64_highFreq.reshape(B, C * self.qmat_64.shape[1] * self.qmat_64.shape[2],(H // self.qmat_64.shape[1]) * (W // self.qmat_64.shape[2]))
+        y_qmat64_highFreq = self.qmat_64_fold(y_qmat64_highFreq)
+
+        return y_qmat64_lowFreq, y_qmat64_highFreq
+
+    def perform_dct(self, x, dct2d, idct2d, qmat, qmat_weights):
+        B,C,H,W=x.shape
+
+        block_size_x=qmat.shape[1]
+        block_size_y=qmat.shape[2]
+
+        learned_qmat = torch.zeros_like(qmat[0])
+        qmat_weights=F.softmax(qmat_weights, dim=0)
+        for i in range(qmat.shape[0]):
+            learned_qmat = learned_qmat + qmat_weights[i] * qmat[i]
+        #learned_qmat = qmat[2]
+        #print(qmat_weights)
+        #exit(0)
+        learned_qmat = learned_qmat.expand([B,C, block_size_x, block_size_y])
+        blocks_dct=dct2d(x)
+        blocks_lowFreq = idct2d(blocks_dct * learned_qmat)
+        blocks_highFreq = idct2d(blocks_dct * (1 - learned_qmat))
+
+        return blocks_lowFreq, blocks_highFreq
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        
+        if(torch.cuda.is_available()):
+            self.device=torch.device("cuda")
+        else:
+            self.device=torch.device("cpu")
+        
+        self.learnableDCTNet=LearnableDCTNet()
+        
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight.data, mode='fan_in', nonlinearity='leaky_relu')
+                if(m.bias!=None):
+                    nn.init.constant_(m.bias.data, 0)
+            elif isinstance(m, nn.GroupNorm):
+                nn.init.constant_(m.weight.data, 1)
+                nn.init.constant_(m.bias.data, 0)
+        
+    
+    def forward(self, x):
+        x=self.learnableDCTNet(x)
+
+        return x
+    
+    
+
---
+++ v1/Models/64_64/Proposed/models/MultiDilaResnet.py
@@ -0,0 +1,119 @@
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+import scipy.io as scio
+
+from models.ResNet import ResNet
+
+
+class CBAMLayer(nn.Module):
+    def __init__(self, channel, reduction=16, spatial_kernel=7):
+        super(CBAMLayer, self).__init__()
+
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        # shared MLP
+        self.mlp = nn.Sequential(
+            nn.Conv2d(channel, channel // reduction, 1, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(channel // reduction, channel, 1, bias=False)
+        )
+        # spatial attention
+        self.conv = nn.Conv2d(2, 1, kernel_size=spatial_kernel,
+                              padding=spatial_kernel // 2, bias=False)
+        self.sigmoid = nn.Sigmoid()
+    def forward(self, x):
+        max_out = self.mlp(self.max_pool(x))
+        avg_out = self.mlp(self.avg_pool(x))
+        channel_out = self.sigmoid(max_out + avg_out)
+        x = channel_out * x
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        spatial_out = self.sigmoid(self.conv(torch.cat([max_out, avg_out], dim=1)))
+        x = spatial_out * x
+        return x
+
+
+class MultiDila_Block(nn.Module):
+    def __init__(self, params):
+        super(MultiDila_Block, self).__init__()
+
+        C=params[0]
+
+        self.cbamLayer=CBAMLayer(C)
+
+        self.dil_conv1=nn.Sequential(
+            nn.Conv2d(in_channels=C, out_channels=C//4, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.GELU(),
+
+            nn.Conv2d(in_channels=C//4, out_channels=C//4, kernel_size=(3, 1), stride=1, dilation=1, padding=(1, 0), bias=False),
+            nn.Conv2d(in_channels=C//4, out_channels=C//4, kernel_size=(1, 3), stride=1, dilation=1, padding=(0, 1), bias=False),
+        )
+
+        self.dil_conv2=nn.Sequential(
+            nn.Conv2d(in_channels=C, out_channels=C//4, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.GELU(),
+
+            nn.Conv2d(in_channels=C//4, out_channels=C//4, kernel_size=(3,1), stride=1, dilation=2, padding=(2,0), bias=False),
+            nn.Conv2d(in_channels=C//4, out_channels=C//4, kernel_size=(1,3), stride=1, dilation=2, padding=(0,2), bias=False),
+        )
+
+        self.dil_conv3=nn.Sequential(
+            nn.Conv2d(in_channels=C, out_channels=C//4, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.GELU(),
+
+            nn.Conv2d(in_channels=C//4, out_channels=C//4, kernel_size=(3,1), stride=1, dilation=3, padding=(3,0), bias=False),
+            nn.Conv2d(in_channels=C//4, out_channels=C//4, kernel_size=(1,3), stride=1, dilation=3, padding=(0,3), bias=False),
+        )
+
+        self.pool=nn.Sequential(
+            nn.AvgPool2d(kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels=C, out_channels=C//4, kernel_size=1, stride=1, padding=0, bias=False),
+        )
+
+        self.ln_nl=nn.Sequential(
+            #nn.BatchNorm2d(C, affine=False),
+            #nn.GroupNorm(max(int(C//4),1), C),
+            nn.LayerNorm([64, 64]),
+            nn.GELU(),
+        )
+
+    def forward(self,x):
+        x = torch.cat((self.dil_conv1(x), self.dil_conv2(x), self.dil_conv3(x), self.pool(x)), dim=1)
+        x = self.ln_nl(x)
+        x = self.cbamLayer(x)
+
+        return x
+
+
+class MultiDilaResnet(nn.Module):
+    def __init__(self, C_in, C_out, num_layers, features=64):
+        super(MultiDilaResnet, self).__init__()
+
+        self.backbone=nn.Sequential(
+            nn.Conv2d(in_channels=C_in, out_channels=features, kernel_size=7, stride=1, padding=3, bias=False),
+            nn.GELU(),
+
+            nn.Conv2d(in_channels=features, out_channels=features, kernel_size=5, stride=1, padding=2, bias=False),
+            nn.GELU(),
+
+            nn.Conv2d(in_channels=features, out_channels=features, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.GELU(),
+        )
+
+        self.resnet=ResNet(num_layers, MultiDila_Block, features)
+
+        self.end = nn.Conv2d(in_channels=features, out_channels=C_out, kernel_size=1, stride=1, padding=0, bias=False)
+
+
+    def forward(self, x):
+        x=self.backbone(x)
+        x=self.resnet(x)
+        x=self.end(x)
+
+        return x
+
+
+
